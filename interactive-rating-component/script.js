64a97b1df61fe2c35c1109195d52987d08dea7ed
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-03-21 05:00:59 +0600

    logic implemented

--- interactive-rating-component/script.js
+++ interactive-rating-component/script.js
@@ -1,14 +1,18 @@
 let feedback = document.querySelector(".feedback");
 let ratedSuccess = document.querySelector(".rated-successful");
 let submitBtn = document.querySelector("button");
+let ratedStar = 0;
+let optionSelected = false;
+let successfulMsg;
 
-optionSelected = false;
-
-let successfulMsg = `<div class="flex justify-center align-items-center ">
+function isRated() {
+  successfulMsg = `<div class="flex justify-center align-items-center ">
           <img src="images/illustration-thank-you.svg" alt="thank you">
         </div>
         <div class="mt-8 flex flex-col  justify-center items-center text-center">
-          <div class="h-8 px-5 bg-gray-700 rounded-3xl text-sm flex items-center text-orange-400"> You selected 4
+          <div class="h-8 px-5 bg-gray-700 rounded-3xl text-sm flex items-center text-orange-400"> You selected ${
+            ratedStar + 1
+          }
             out
             of 5
           </div>
@@ -18,23 +22,18 @@ let successfulMsg = `<div class="flex justify-center align-items-center ">
             support,
             don’t hesitate to get in touch!</p>
         </div>`;
-
+}
 submitBtn.addEventListener("click", () => {
-  optionSelected = isRated();
-
   if (optionSelected) {
     feedback.remove();
+    isRated();
     ratedSuccess.innerHTML = successfulMsg;
   } else {
     console.log("Rate First");
   }
 });
 
-function isRated() {
-  return optionSelected;
-}
 let ratingOptions = document.querySelectorAll("[id='rating']");
-let lastSelectedidx = 0;
 ratingOptions.forEach((option, index) => {
   option.addEventListener("click", () => {
     const isSelected = option.classList.contains("bg-orange-500");
@@ -42,11 +41,13 @@ ratingOptions.forEach((option, index) => {
     if (isSelected) {
       // If the clicked option is already selected, remove styles from all options
       ratingOptions.forEach((opt) => {
+        optionSelected = false;
         opt.classList.add("hover:bg-gray-600");
         opt.classList.remove("bg-orange-500");
         opt.classList.remove("text-white");
       });
-      if (index != lastSelectedidx) {
+      if (index != ratedStar) {
+        optionSelected = true;
         // If the clicked option is selected but different then apply styles up to the clicked index
         for (let i = 0; i <= index; i++) {
           ratingOptions[i].classList.remove("hover:bg-gray-600");
@@ -54,15 +55,16 @@ ratingOptions.forEach((option, index) => {
           ratingOptions[i].classList.add("text-white");
         }
       }
-      lastSelectedidx = index;
+      ratedStar = index;
     } else {
+      optionSelected = true;
       // If the clicked option is not selected, apply styles up to the clicked index
       for (let i = 0; i <= index; i++) {
         ratingOptions[i].classList.remove("hover:bg-gray-600");
         ratingOptions[i].classList.add("bg-orange-500");
         ratingOptions[i].classList.add("text-white");
       }
-      lastSelectedidx = index;
+      ratedStar = index;
     }
   });
 });
