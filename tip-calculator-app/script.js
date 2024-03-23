4c75f8f50c014a138a90e7bee74894d45b23d768
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-03-23 15:45:07 +0600

    responsive design ready

---
+++ tip-calculator-app/script.js
@@ -0,0 +1,7 @@
+let bill = document.getElementById("bill");
+let person = document.getElementById("person");
+let tipAmount = document.getElementById();
+let customTip = document.getElementById("custom-tip");
+let personInvalid = document.getElementById("person-invalid");
+
+window.addEventListener("load", () => {});
---
+++ tip-calculator-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      spacemono: ["Space Mono", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
