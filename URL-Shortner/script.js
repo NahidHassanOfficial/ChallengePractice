0382854f6719441beafd348a451b2b182f64e643
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-07-12 21:19:11 +0600

    Implement session storage for URLs

--- URL-Shortner/script.js
+++ URL-Shortner/script.js
@@ -6,6 +6,33 @@ let shortenResultsDiv = document.getElementById("shorten-urls");
 let userUrl = "";
 let generatedUrl = "";
 
+let currentSessionUrls = [];
+
+//Retrive session storage
+(() => {
+  let storedUrls = sessionStorage.getItem("ShortlyUrls");
+  if (storedUrls) {
+    currentSessionUrls = JSON.parse(storedUrls);
+    currentSessionUrls.forEach((url) => {
+      shortenUrl = `<div class="w-full  bg-white rounded-md h-fit lg:h-14 p-5 text-sm flex flex-col gap-2 lg:gap-0 lg:flex-row justify-between items-start lg:items-center">
+      <p class="text-gray-600 break-all">${url.ShortenUrl}</p>
+      <div class="w-full lg:w-fit flex justify-between lg:space-x-3">
+        <p class="text-[#2bd1cf]">${url.generatedUrl}</p>
+        <button
+          class="bg-[#2bd1cf] hover:bg-[#9ce2e2] w-fit h-fit px-3 py-1 rounded-md text-xs text-white"
+          id="copy-btn"
+        >
+          Copy
+        </button>
+      </div>
+    </div>`;
+
+      shortenResultsDiv.insertAdjacentHTML("afterbegin", shortenUrl);
+    });
+  }
+})();
+
+//copy url function with clicked effect
 function copyUrl() {
   let copyBtns = document.querySelectorAll("[id='copy-btn']");
   copyBtns.forEach(function (btn) {
@@ -17,12 +44,24 @@ function copyUrl() {
   });
 }
 
+//store urls in session
+function StoreCurrentSessionUrls(userUrl, generatedUrl) {
+  currentSessionUrls.push({
+    ShortenUrl: userUrl,
+    generatedUrl: generatedUrl,
+  });
+
+  sessionStorage.setItem("ShortlyUrls", JSON.stringify(currentSessionUrls));
+}
+
 async function generateUrl(userUrl) {
   try {
     let response = await fetch(
       `https://tinyurl.com/api-create.php?url=${userUrl}`
     );
     generatedUrl = await response.text();
+
+    StoreCurrentSessionUrls(userUrl, generatedUrl);
 
     shortenUrl = `<div
           class="w-full  bg-white rounded-md h-fit lg:h-14 p-5 text-sm flex flex-col gap-2 lg:gap-0 lg:flex-row justify-between items-start lg:items-center">
@@ -35,6 +74,7 @@ async function generateUrl(userUrl) {
         </div>`;
 
     shortenResultsDiv.insertAdjacentHTML("afterbegin", shortenUrl);
+
     //visit copy function
     copyUrl();
   } catch (error) {
@@ -42,6 +82,7 @@ async function generateUrl(userUrl) {
   }
 }
 
+//input validation check
 document.addEventListener("DOMContentLoaded", function () {
   getUrlBtn.addEventListener("click", function () {
     let url = inputUrl.value;
